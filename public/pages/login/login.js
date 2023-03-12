620fc785fe7b8171b94d3da37947eea109a3dd43
SkibstedN <niko933n@stud.kea.dk>
2023-03-12 22:10:21 +0100

    login page edits

--- public/pages/login/login.js
+++ public/pages/login/login.js
@@ -1,19 +1,19 @@
 
 
 // Retrieve the form and attach a submit event listener
-const loginForm = document.querySelector('form');
-loginForm.addEventListener('submit', (event) => {
+const loginForm = document.querySelector("form");
+loginForm.addEventListener("submit", (event) => {
   event.preventDefault(); // prevent the form from submitting normally
 
   const name = "noname";
-const password = "noname";
+  const password = "noname";
   
-  const usernameInput = document.querySelector('#username');
-  const passwordInput = document.querySelector('#password');
+  const usernameInput = document.querySelector("#username");
+  const passwordInput = document.querySelector("#password");
   
   if (usernameInput.value === name && passwordInput.value === password) {
     window.location.href = "/frontpage";
   } else {
-    alert('Incorrect username or password!');
+    alert("Incorrect username or password!");
   }
 });
